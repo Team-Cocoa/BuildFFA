f8e6c50c2eed22abc0c20e30a09e8c615807bb28
fixca <jaehoon1744@gmail.com>
2021-06-04 00:53:11 +0900

    2021/06/02/00:52

--- src/main/java/kr/teamcocoa/buildffa/listener/InventoryDragListener.java
+++ src/main/java/kr/teamcocoa/buildffa/listener/InventoryDragListener.java
@@ -1,5 +1,6 @@
 package kr.teamcocoa.buildffa.listener;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -7,10 +8,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryDragEvent;
 
 public class InventoryDragListener implements Listener {
+
     @EventHandler
     public void onDrag(InventoryDragEvent e){
         if(e.getWhoClicked() instanceof Player){
-            Player player = (Player)e.getWhoClicked();
             if(e.getInventory().getName().equals(ChatColor.translateAlternateColorCodes('&', "&cInventorySorting"))){
                 e.setCancelled(true);
             }
