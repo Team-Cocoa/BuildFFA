98b4243d45b0005d68c6f7bcbd5af905570241b0
fixca <jaehoon1744@gmail.com>
2022-02-01 02:53:20 +0900

    2022/02/01/02:53

--- src/main/java/kr/teamcocoa/buildffa/utils/MYSQL.java
+++ src/main/java/kr/teamcocoa/buildffa/utils/MYSQL.java
@@ -1,6 +1,5 @@
 package kr.teamcocoa.buildffa.utils;
 
-import org.bukkit.Bukkit;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -24,7 +23,7 @@ public class MYSQL {
   public void connect() {
     if (!isConnected())
       try {
-        con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, username, password);
+        con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database + "?validationQuery=select 1&testWhileIdle=true&timeBetweenEvictionRunsMillis=30000", username, password);
         System.out.println("| [BuildFFA] Successfully connected to MYSQL!                 |");
       } catch (SQLException e) {
         System.out.println("| [BuildFFA] Failed to connect to MYSQL server.               |");
