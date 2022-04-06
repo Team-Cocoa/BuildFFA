9990d55fb72a349d3c0ae410b68065b38ffbfbf0
fixca <jaehoon1744@gmail.com>
2022-04-07 01:12:19 +0900

    2022/04/07/01:12

--- src/main/java/kr/teamcocoa/buildffa/listener/PlayerDeathListener.java
+++ src/main/java/kr/teamcocoa/buildffa/listener/PlayerDeathListener.java
@@ -10,6 +10,8 @@ public class PlayerDeathListener implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent e) {
         e.setDeathMessage(null);
+        e.setKeepInventory(true);
+        e.setKeepLevel(true);
         if (Main.playerData.containsKey(e.getEntity())) {
             Main.playerData.get(e.getEntity()).death(false);
         }
